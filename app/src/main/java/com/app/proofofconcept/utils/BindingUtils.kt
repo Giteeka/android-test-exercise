5a399f13cb6206507ecbb0df7d7d844044e4ed4a
unknown <gitsawlani@gmail.com>
2019-09-11 08:00:48 +0530

    -improvements for row file display

--- app/src/main/java/com/app/proofofconcept/utils/BindingUtils.kt
+++ app/src/main/java/com/app/proofofconcept/utils/BindingUtils.kt
@@ -1,21 +1,31 @@
 package com.app.proofofconcept.utils
 
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.app.proofofconcept.GlideApp
+import com.app.proofofconcept.R
 
 @BindingAdapter("setImagePath")
 fun AppCompatImageView.setImagePath(path: String?) {
-    if (path.isNullOrBlank()) {
-        GlideApp.with(this.context).load(this.drawable).into(this)
-    } else {
-        GlideApp.with(this.context).load(path).into(this)
-    }
+        GlideApp.with(this.context).load(path).placeholder(R.drawable.ic_image_black_24dp).into(this)
+}
 
-    @BindingAdapter("setRefreshing")
-    fun SwipeRefreshLayout.setRefreshing(boolean: Boolean) {
-        isRefreshing = boolean
-    }
+@BindingAdapter("setRefreshing")
+fun SwipeRefreshLayout.setRefreshing(boolean: Boolean) {
+    isRefreshing = boolean
+}
+
+@BindingAdapter(value = ["rowText", "defaultText"], requireAll = false)
+//    @BindingAdapter("defaultText")
+fun setTextViewText(view: TextView, rowText: String?, defaultText: String?) {
+    Logg.e("BindingAdapter", "text : $rowText")
+    Logg.e("BindingAdapter", "defaultText : $defaultText")
+    if (rowText.isNullOrEmpty() ||rowText.equals("null", ignoreCase = false))
+        view.text = defaultText
+    else
+        view.text = rowText
 
 }
+
